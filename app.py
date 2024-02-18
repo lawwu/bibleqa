1c67d7fee6b0b709c8f7e968b51c88f380d6c1f0
Lawrence Wu <lawrencewu1@gmail.com>
2024-02-18 00:05:41 -0800

    use secrets.toml

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_PROJECT"] = "bibleqa"
 
 path_to_index = ".ragatouille/colbert/indexes/ESV/"
 RAG = RAGPretrainedModel.from_index(path_to_index)
